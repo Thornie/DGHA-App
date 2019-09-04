61f8fd37bfbddd8b0dd857999e78448970dce678
Lee Chu Yem <leechuyem@gmail.com>
2019-09-04 11:15:33 +1000

    implemented RouteGenerator

---
+++ lib/misc/route_generator.dart
@@ -0,0 +1,19 @@
+import 'package:dgha_brochure/models/screen_args.dart';
+import 'package:dgha_brochure/screens/info_screens.dart';
+import 'package:dgha_brochure/screens/menu_screen.dart';
+import 'package:flutter/material.dart';
+
+class RouteGenerator{
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final ScreenArguments arguments = settings.arguments; 
+
+    switch(settings.name) {
+      case InfoScreen.id:
+        return MaterialPageRoute(builder: (_) => InfoScreen(appBarTitle: arguments.title, texts: arguments.texts,)); 
+      case PageViewMenu.id:
+        return MaterialPageRoute(builder: (_) => PageViewMenu()); 
+      default:
+        return MaterialPageRoute(builder: (_) => PageViewMenu()); 
+    }
+  }
+}
