e925f8f056cb588e8496664afb5a03f57e64c80d
Lee Chu Yem <leechuyem@gmail.com>
2019-09-05 01:12:22 +1000

    removed margin bcs margin is now just 0

--- lib/components/menu_heading.dart
+++ lib/components/menu_heading.dart
@@ -3,14 +3,12 @@ import 'package:flutter/material.dart';
 
 class InfoMenuHeading extends StatelessWidget {
   final String text; 
-  final double topMargin; 
   
-  InfoMenuHeading({this.text, this.topMargin});
+  InfoMenuHeading({this.text});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: this.topMargin),
       width: double.maxFinite,
       padding: EdgeInsets.only(left: 30),
       child: Text(
