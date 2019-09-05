319626f93391426502d293d776fbaea96cff04f5
Lee Chu Yem <leechuyem@gmail.com>
2019-09-05 17:14:35 +1000

    extracted the expansionTile into its own widget

---
+++ lib/components/menu_expansion_tile.dart
@@ -0,0 +1,56 @@
+import 'package:dgha_brochure/components/menu_tile.dart';
+import 'package:dgha_brochure/misc/styles.dart';
+import 'package:dgha_brochure/models/menu_tile_data.dart';
+import 'package:flutter/material.dart';
+
+class MenuExpansionTile extends StatefulWidget {
+  final MenuTileData tile;
+  MenuExpansionTile({this.tile});
+
+  @override
+  _MenuExpansionTileState createState() => _MenuExpansionTileState();
+}
+
+class _MenuExpansionTileState extends State<MenuExpansionTile> {
+  bool showIcon = true; 
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 20),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Styles.buildIcon(widget.tile.icon, Styles.yellow),
+          ),
+          SizedBox(width: 5,),
+          Expanded(
+            child: ExpansionTile(
+              onExpansionChanged: (opened) {
+                setState(() {
+                 showIcon = !showIcon; 
+                });
+              },
+              title: Text(
+                widget.tile.title,
+                style: Styles.pTextStyle,
+              ),
+              children: _buildChildren(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  List<Widget> _buildChildren() {
+    List<Widget> children = new List<Widget>();
+    for(int i = 0; i < widget.tile.children.length; i++) {
+      Widget w = MenuTile(tile: widget.tile.children[i],); 
+      children.add(w);
+    }
+    return children; 
+  }
+}
