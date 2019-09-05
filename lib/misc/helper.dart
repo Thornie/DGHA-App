4b58938d57f99abf20a5d0564f4a26cc633ffda3
Lee Chu Yem <leechuyem@gmail.com>
2019-09-05 21:06:34 +1000

    renamed asset path

--- lib/misc/helper.dart
+++ lib/misc/helper.dart
@@ -3,7 +3,7 @@ import 'dart:convert' show utf8;
 
 class Helper {
   Future<String> loadAsset(BuildContext _context, fileName) async {
-    var string = await DefaultAssetBundle.of(_context).loadString('assets/$fileName');
+    var string = await DefaultAssetBundle.of(_context).loadString(fileName);
     var encoded = utf8.encode(string);
     var decoded = utf8.decode(encoded);
     return decoded; 
