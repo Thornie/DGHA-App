0b841b143c7f3b15f82f7593b4721519c80e0713
Lee Chu Yem <leechuyem@gmail.com>
2019-09-05 17:15:43 +1000

    made colour a parameter in BuildIcon

--- lib/misc/styles.dart
+++ lib/misc/styles.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
 class Styles {
+  // ----------------- COLOURS
+  static final Color yellow = Color(0xfffad00c);
+  static final Color transparent = Color(0x00000000);
+
   // ----------------- APP BAR
-  static final EdgeInsets customAppBarPadding =
-      EdgeInsets.only(right: 10, bottom: 5, top: 10);
+  static final EdgeInsets customAppBarPadding = EdgeInsets.only(right: 10, bottom: 5, top: 10);
+
   static final BoxDecoration customAppBarBoxDecoration = BoxDecoration(
-      color: Colors.white,
-      borderRadius: BorderRadius.only(bottomRight: Radius.circular(20), bottomLeft: Radius.circular(20)),
-      boxShadow: [customBoxShadow(Offset(0, 1))]);
+    color: Colors.white,
+    borderRadius: BorderRadius.only(bottomRight: Radius.circular(20), bottomLeft: Radius.circular(20)),
+    boxShadow: [customBoxShadow(Offset(0, 1))],
+  );
+
   static final TextStyle customAppBartextStyle = TextStyle(
     fontFamily: "RedHatText",
     fontWeight: FontWeight.w700,
@@ -16,14 +22,17 @@ class Styles {
   );
 
   // ----------------- GENERAL TEXT
-  static TextStyle generalTextStyle = TextStyle(
+  static TextStyle pTextStyle = TextStyle(
     fontFamily: "Manjari",
     fontSize: 20,
     color: Color(0xff102d4d),
   );
 
   static TextStyle h1TextStyle = TextStyle(
-      fontFamily: "RedHatText", fontSize: 20, fontWeight: FontWeight.w500);
+    fontFamily: "RedHatText", 
+    fontSize: 20, 
+    fontWeight: FontWeight.w500,
+  );
   
   static TextStyle h2TextStyle = TextStyle(
     fontFamily: "Manjari",
@@ -32,8 +41,8 @@ class Styles {
   ); 
 
   // ----------------- ICON
-  static Icon buildAppBarIcon(IconData icon) {
-    return Icon(icon, color: Color(0xfffad00c), size: 30);
+  static Icon buildIcon(IconData icon, Color colour) {
+    return Icon(icon, color: colour, size: 30);
   }
 
   // ----------------- MISC
