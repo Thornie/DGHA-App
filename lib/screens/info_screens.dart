91d8fc19c51bec439b985995135ecbc0e00ea5e9
Lee Chu Yem <leechuyem@gmail.com>
2019-09-10 18:51:25 +1000

    made the body into stack

--- lib/screens/info_screens.dart
+++ lib/screens/info_screens.dart
@@ -4,7 +4,6 @@ import 'package:dgha_brochure/misc/helper.dart';
 import 'package:dgha_brochure/misc/styles.dart';
 import 'package:dgha_brochure/models/languages.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InfoScreen extends StatefulWidget {
   static const String id = "Info Screen";
@@ -19,19 +18,7 @@ class InfoScreen extends StatefulWidget {
 
 class _InfoScreenState extends State<InfoScreen> {
   String infoText = "";
-
-  // NOTE: App Properties
-  double srcWidth;
-  double srcHeight;
-  double horizontalPadding = 30;
-
-  // NOTE: App Bar Properties
-  double appBarHeight;
-  double appBarRadius;
-
-  // Pop Menu
-
-  // NOTE: Text
+  double scrHeight;
 
   @override
   void initState() {
@@ -40,18 +27,7 @@ class _InfoScreenState extends State<InfoScreen> {
   }
 
   void calcDimensions(Orientation orientation) {
-    // NOTE: App
-    this.srcWidth = MediaQuery.of(context).size.width;
-    this.srcHeight = MediaQuery.of(context).size.height;
-
-    // NOTE: App Bar
-    this.appBarHeight = orientation == Orientation.portrait ? srcHeight / 12 : srcWidth / 12;
-    // this.appBarTextScale = this.appBarHeight / 70;
-    this.appBarRadius = this.appBarHeight / 3.5;
-
-    // NOTE: Popmenu
-    // this.popMenuTextScale = this.appBarHeight / 90 < 1.2 ? this.appBarHeight / 90 : 1.2;
-    // this.textScale = this.appBarHeight / 90 < 1.2 ? this.appBarHeight / 90 : 1.2;
+    this.scrHeight = MediaQuery.of(context).size.height;
   }
 
   void setLang(int index) {
@@ -67,59 +43,63 @@ class _InfoScreenState extends State<InfoScreen> {
     return Scaffold(
       body: SafeArea(child: OrientationBuilder(
         builder: (context, orientation) {
-          this.calcDimensions(orientation);
-          return Column(
+          return Stack(
             children: <Widget>[
+              Container(
+                height: this.scrHeight,
+                child: ListView(
+                  physics: BouncingScrollPhysics(),
+                  children: <Widget>[
+                    SizedBox(
+                      height: 70,
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: Styles.spacing),
+                      child: SingleChildScrollView(
+                        physics: BouncingScrollPhysics(),
+                        child: Text("\n" + infoText + "\n", style: Styles.pStyle),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               DghaAppBar(
-                text: widget.appBarTitle,
-                appBarHeight: this.appBarHeight,
-                srcWidth: this.srcWidth,
-                horizontalPadding: this.horizontalPadding,
-                borderRadius: this.appBarRadius,
-                isMenuScr: false,
-                leftChild: Semantics(
-                  label: "Back Button",
-                  hint: "Double tap to go back to home screen",
+                childOne: Container(
                   child: GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).pop();
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: DghaIcon(
+                      icon: Icons.arrow_back_ios,
+                    ),
+                  ),
+                ),
+                childTwo: Container(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: PopupMenuButton(
+                      onSelected: (choice) {
+                        int newLangIndex = widget.texts.indexWhere((lang) => lang.languageName == choice);
+                        setLang(newLangIndex);
                       },
-                      child: DghaIcon(icon: Icons.arrow_back_ios)),
-                ),
-                rightChid: Semantics(
-                  label: "Translation Button",
-                  hint: "Double to tap to open up translation menu",
-                  child: PopupMenuButton(
-                    onSelected: (choice) {
-                      int newLangIndex = widget.texts.indexWhere((lang) => lang.languageName == choice);
-                      setLang(newLangIndex);
-                    },
-                    child: DghaIcon(icon: Icons.translate),
-                    itemBuilder: (BuildContext ctxt) {
-                      return widget.texts.map((Language lang) {
-                        return PopupMenuItem(
-                          value: lang.languageName,
-                          child: Semantics(
-                            hint: "Double tap to selected ${lang.languageName} translation.",
-                            child: Text(lang.languageName, style: Styles.h3LinkStyle),
-                          ),
-                        );
-                      }).toList();
-                    },
+                      child: DghaIcon(icon: Icons.translate),
+                      itemBuilder: (BuildContext ctxt) {
+                        return widget.texts.map((Language lang) {
+                          return PopupMenuItem(
+                            value: lang.languageName,
+                            child: Semantics(
+                              hint: "Double tap to selected ${lang.languageName} translation.",
+                              child: Text(lang.languageName, style: Styles.h3LinkStyle),
+                            ),
+                          );
+                        }).toList();
+                      },
+                    ),
                   ),
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  width: double.maxFinite,
-                  height: double.maxFinite,
-                  margin: EdgeInsets.symmetric(horizontal: this.horizontalPadding),
-                  child: SingleChildScrollView(
-                    physics: BouncingScrollPhysics(),
-                    child: Text("\n" + infoText + "\n", style: Styles.pStyle),
-                  ),
-                ),
-              ),
+                text: widget.appBarTitle,
+                isMenu: false,
+              )
             ],
           );
         },
