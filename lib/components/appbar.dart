20d3cdd464d694083798855c5a9d2992dbde637b
Lee Chu Yem <leechuyem@gmail.com>
2019-09-10 18:49:31 +1000

    height and width determined by children

--- lib/components/appbar.dart
+++ lib/components/appbar.dart
@@ -2,49 +2,39 @@ import 'package:dgha_brochure/misc/styles.dart';
 import 'package:flutter/material.dart';
 
 class DghaAppBar extends StatelessWidget {
-  final double srcWidth;
-  final double appBarHeight;
-  final double horizontalPadding;
-  final Widget leftChild;
-  final Widget rightChid;
-  final String text;
-  final double borderRadius;
-  final bool isMenuScr;
+  final Widget childOne;
+  final Widget childTwo;
+  final String text; 
+  final bool isMenu; 
 
-  DghaAppBar(
-      {this.srcWidth, this.appBarHeight, this.horizontalPadding, this.leftChild, this.text, this.rightChid, this.borderRadius, this.isMenuScr});
+  DghaAppBar({this.childOne, this.childTwo, this.text, this.isMenu}); 
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: this.srcWidth,
-      height: this.appBarHeight,
-      margin: EdgeInsets.only(bottom: 3),
-      padding: EdgeInsets.symmetric(horizontal: Styles.appBarHorizontalPadding),
-      constraints: BoxConstraints(minHeight: 70, maxHeight: 125),
+      width: double.maxFinite,
+      padding: EdgeInsets.symmetric(horizontal: 10),
       decoration: BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(this.borderRadius), bottomRight: Radius.circular(this.borderRadius)),
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(Styles.normalRadius),
+            bottomRight: Radius.circular(Styles.normalRadius),
+          ),
           boxShadow: [BoxShadow(color: Styles.grey, blurRadius: 3, offset: Offset(0, 3))]),
       child: Row(
         children: <Widget>[
-          this.leftChild,
+          this.childOne,
           Expanded(
-            child: Semantics(
-              label: "Title",
-              hint: "D G H A. Dog Guide Handler Australia",
-              child: Container(
-                // color: Colors.blue,
-                padding: EdgeInsets.all(this.appBarHeight / 8),
-                child: Text(
-                  this.text,
-                  style: isMenuScr ? Styles.h1Style : Styles.h2Style,
-                  textAlign: TextAlign.center,
-                ),
+            child: Container(
+              child: Text(
+                this.text,
+                style: this.isMenu ? Styles.h1Style : Styles.h2Style,
+                textAlign: TextAlign.center,
+                textScaleFactor: 1,
               ),
             ),
           ),
-          this.rightChid
+          this.childTwo
         ],
       ),
     );
