0b5b33a97b5ef12a001ed8d7574df72f2e269a34
Lee Chu Yem <leechuyem@gmail.com>
2019-09-10 18:51:36 +1000

    new width and height

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -5,8 +5,8 @@ import 'package:dgha_brochure/components/menu_card.dart';
 import 'package:dgha_brochure/misc/data.dart';
 import 'package:dgha_brochure/misc/styles.dart';
 import 'package:dgha_brochure/models/menu_card_data.dart';
-import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/material.dart';
 
 class MenuScreen extends StatefulWidget {
   static const String id = "Menu Screen";
@@ -18,67 +18,51 @@ class _MenuScreenState extends State<MenuScreen> {
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   // NOTE: App Properties
-  double srcWidth;
-  double srcHeight;
-  double horizontalPadding = 30;
-
-  // NOTE: App Bar Properties
-  double appBarHeight;
-  double appBarRadius;
+  double scrWidth;
+  double scrHeight;
+  double textScale;
 
   // NOTE: Drawer
   double drawerWidth;
 
   // NOTE: Card Properties
   bool isVertical = false;
-  double cardSpacing = 20;
+
   double cardWidth;
   double cardHeight;
-  double cardMaxHeight;
-  double cardBorderRadius;
+  double cardMinHeight;
+  double cardMinWidth = 160;
+
+  // NOTE: Card Text
+  final double textMinWidth = 102;
+  final double textMinHeight = 34;
 
   void calcDimensions(Orientation orientation) {
-    // NOTE: App
-    this.srcWidth = MediaQuery.of(context).size.width;
-    this.srcHeight = MediaQuery.of(context).size.height;
+    this.scrWidth = MediaQuery.of(context).size.width;
+    this.scrHeight = MediaQuery.of(context).size.height;
+    this.textScale = MediaQuery.of(context).textScaleFactor;
 
-    // NOTE: App Bar
-    this.appBarHeight = orientation == Orientation.portrait ? srcHeight / 12 : srcWidth / 12;
-    // this.appBarTextScale = this.appBarHeight / 70;
-    this.appBarRadius = this.appBarHeight / 3.5;
+    double cardNewMinWidth = (this.textMinWidth * this.textScale) + Styles.textPadding * 2;
+    double cardNewWidth = 0;
 
-    // NOTE: Drawer
-    this.drawerWidth = orientation == Orientation.portrait ? srcWidth * 0.8 : srcHeight * 0.8;
-    // this.tileTextScale = drawerWidth / 440 < 1.5 ? drawerWidth / 440 : 1.5;
+    if (isVertical) {
+      int numOfCards = ((this.scrWidth - (Styles.spacing * 2)) / cardNewMinWidth).floor();
 
-    // NOTE: Card
-    if (this.isVertical && orientation == Orientation.portrait) {
-      this.cardWidth = (this.srcWidth - this.horizontalPadding * 2 - this.cardSpacing) / 2;
-      this.cardHeight = this.srcHeight / 3.5;
+      while (cardNewWidth < cardNewMinWidth) {
+        double scrWidthIncludingPadding = scrWidth - (Styles.spacing * 2) - (Styles.spacing * (numOfCards - 1));
+        cardNewWidth = scrWidthIncludingPadding / (numOfCards);
+        numOfCards--;
+      }
+    } else {
+      cardNewWidth = cardNewMinWidth + Styles.spacing;
+      if (cardNewWidth < this.cardMinWidth) {
+        cardNewWidth = this.cardMinWidth;
+      }
     }
 
-    if (this.isVertical && orientation == Orientation.landscape) {
-      this.cardWidth = (srcWidth - horizontalPadding * 2 - cardSpacing * 3) / 4;
-      this.cardHeight = srcHeight / 2.25;
-    }
-
-    if (!this.isVertical && orientation == Orientation.portrait) {
-      this.cardWidth = (srcWidth - horizontalPadding * 2 - cardSpacing * 2) / 2.1;
-      this.cardHeight = this.srcHeight / 3.5;
-      this.cardMaxHeight = this.cardWidth * 1.1;
-      this.cardBorderRadius = this.cardWidth / 8;
-      // this.cardTextScale = this.cardWidth / 160;
-    }
-
-    // NOTE: the srcHeight and srcWidth are flipped from the Orientatin.portrait
-    if (!this.isVertical && orientation == Orientation.landscape) {
-      this.cardWidth = (srcHeight - horizontalPadding * 2 - cardSpacing * 2) / 2.1;
-      this.cardHeight = this.srcWidth / 3.5;
-    }
-
-    this.cardMaxHeight = this.cardWidth * 1.1;
-    this.cardBorderRadius = this.cardWidth / 8;
-    // this.cardTextScale = this.cardWidth / 150 < 1.2 ? this.cardWidth / 200 : 1.2;
+    this.drawerWidth = this.scrWidth * 0.75;
+    this.cardWidth = cardNewWidth;
+    this.cardHeight = this.cardWidth * 1.15;
   }
 
   @override
@@ -87,90 +71,76 @@ class _MenuScreenState extends State<MenuScreen> {
       key: _scaffoldKey,
       drawer: MenuDrawer(
         width: this.drawerWidth,
-        borderRadius: this.appBarRadius,
-        marginLeft: this.horizontalPadding,
-        iconSize: this.appBarHeight,
       ),
       body: SafeArea(
         child: OrientationBuilder(
           builder: (context, orientation) {
             this.calcDimensions(orientation);
-            return Column(
+            return Stack(
               children: <Widget>[
-                // NOTE: APPBAR
-                DghaAppBar(
-                    text: "DGHA",
-                    appBarHeight: this.appBarHeight,
-                    srcWidth: this.srcWidth,
-                    horizontalPadding: this.horizontalPadding,
-                    borderRadius: this.appBarRadius,
-                    isMenuScr: true,
-                    leftChild: Semantics(
-                      label: "Menu button",
-                      hint: "Double tap to open up side bar navigation",
-                      excludeSemantics: true,
-                      child: GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              _scaffoldKey.currentState.openDrawer();
-                            });
-                          },
-                          child: DghaIcon(
-                            icon: FontAwesomeIcons.bars,
-                          )),
-                    ),
-                    rightChid: Semantics(
-                      label: "Card Direction Button",
-                      hint: this.isVertical ? "Double tap to list cards horizontally" : "Double tap to list cards vertically",
-                      excludeSemantics: true,
-                      child: GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              this.isVertical = !this.isVertical;
-                            });
-                          },
-                          child: DghaIcon(
-                            icon: this.isVertical ? FontAwesomeIcons.arrowsAltH : FontAwesomeIcons.arrowsAltV,
-                          )),
-                    )),
-                // NOTE: TILES AREA
-                Expanded(
-                  child: SingleChildScrollView(
+                Container(
+                  height: this.scrHeight,
+                  child: ListView(
                     physics: BouncingScrollPhysics(),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Semantics(
-                          label: "Sub Heading. ",
-                          hint: "There are two cards in this category. Double tap on the card to read more about the topic",
-                          child: Container(
-                            width: double.maxFinite,
-                            padding: EdgeInsets.only(left: this.horizontalPadding, top: 30, bottom: 10),
-                            child: Text("General Information", style: Styles.h2Style),
-                          ),
+                    children: <Widget>[
+                      SizedBox(
+                        height: 105,
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: Styles.spacing),
+                        child: Text(
+                          "General Information",
+                          style: Styles.h2Style,
                         ),
-                        Container(
-                          height: cardHeight,
-                          constraints: BoxConstraints(maxHeight: cardMaxHeight),
-                          child: _buildCardList(Data.generalInfoCardData),
+                      ),
+                      _buildCardsList(Data.generalInfoCardData),
+                      SizedBox(
+                        height: isVertical ? 40 : 0,
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: Styles.spacing),
+                        child: Text(
+                          "Laws",
+                          style: Styles.h2Style,
                         ),
-                        Semantics(
-                          label: "Sub Heading. ",
-                          hint: "There are 9 cards in this category. " +
-                              (this.isVertical ? "Slide up and down the list below to see more cards. " : "Slide left to right the list below to see more cards. ") +
-                              "Double tap on the card to read more about the topic",
-                          child: Container(
-                            width: double.maxFinite,
-                            padding: EdgeInsets.only(left: this.horizontalPadding, top: 30, bottom: 10),
-                            child: Text("Federal and State Laws", style: Styles.h2Style),
-                          ),
-                        ),
-                        _buildLawCards(orientation),
-                        SizedBox(height: this.cardSpacing),
-                      ],
-                    ),
+                      ),
+                      _buildCardsList(Data.lawInfoCardData),
+                      SizedBox(
+                        height: 40,
+                      ),
+                    ],
                   ),
                 ),
+
+                // APP BAR
+                DghaAppBar(
+                  childOne: Container(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _scaffoldKey.currentState.openDrawer();
+                        });
+                      },
+                      child: DghaIcon(
+                        icon: FontAwesomeIcons.bars,
+                      ),
+                    ),
+                  ),
+                  childTwo: Container(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          this.isVertical = !this.isVertical;
+                        });
+                      },
+                      child: DghaIcon(
+                        icon: this.isVertical ? FontAwesomeIcons.arrowsAltH : FontAwesomeIcons.arrowsAltV,
+                      ),
+                    ),
+                  ),
+                  text: "DGHA",
+                  isMenu: true,
+                )
               ],
             );
           },
@@ -179,79 +149,42 @@ class _MenuScreenState extends State<MenuScreen> {
     );
   }
 
-  EdgeInsets getMargin(int length, int index) {
-    return index == length - 1 ? EdgeInsets.all(0) : EdgeInsets.only(right: cardSpacing);
-  }
+  // VERTICAL
+  List<Widget> _buildCards(List<MenuCardData> cardsData, bool isSeparated) {
+    List<Widget> cards = new List<Widget>();
 
-  ListView _buildCardList(List<MenuCardData> cardList) {
-    return ListView.builder(
-      scrollDirection: Axis.horizontal,
-      physics: BouncingScrollPhysics(),
-      padding: EdgeInsets.fromLTRB(this.horizontalPadding, 0, this.horizontalPadding, 0),
-      itemCount: cardList.length,
-      itemBuilder: (BuildContext context, int index) {
-        String upAndDown = "${cardList[index].semanticHint}. Or slide up and down to see more cards"; 
-        String leftAndRight = "${cardList[index].semanticHint}. Or slide left and right to see more cards";
-        return Container(
-          margin: getMargin(cardList.length, index),
-          child: Semantics(
-            label: cardList[index].semanticLabel,
-            hint: this.isVertical ? upAndDown : leftAndRight,
-            child: MenuCard(
-              card: cardList[index],
-              width: this.cardWidth,
-              height: this.cardHeight,
-              maxHeight: this.cardMaxHeight,
-              radius: this.cardBorderRadius,
-            ),
-          ),
-        );
-      },
-    );
-  }
+    for (int i = 0; i < cardsData.length; i++) {
+      Widget w = MenuCard(card: cardsData[i], cardWidth: this.cardWidth, cardHeight: this.cardHeight);
+      cards.add(w);
 
-  Widget _buildLawCards(Orientation orientation) {
-    if (this.isVertical && orientation == Orientation.portrait) {
-      return _verticalAndPortWidgets();
-    } else if (this.isVertical && orientation == Orientation.landscape) {
-      return _verticalAndLandWidgets();
-    } else {
-      return _horizontalWidgets();
+      if ((isSeparated) && (i < cardsData.length - 1)) {
+        Widget separator = SizedBox(width: Styles.spacing);
+        cards.add(separator);
+      }
     }
+    return cards;
   }
 
-  Widget _verticalAndPortWidgets() {
-    return Column(
-      children: <Widget>[
-        Container(height: cardHeight, constraints: BoxConstraints(maxHeight: cardMaxHeight), child: _buildCardList(Data.lawInfoCardDataPortRow1)),
-        SizedBox(height: this.cardSpacing),
-        Container(height: cardHeight, constraints: BoxConstraints(maxHeight: cardMaxHeight), child: _buildCardList(Data.lawInfoCardDataPortRow2)),
-        SizedBox(height: this.cardSpacing),
-        Container(height: cardHeight, constraints: BoxConstraints(maxHeight: cardMaxHeight), child: _buildCardList(Data.lawInfoCardDataPortRow3)),
-        SizedBox(height: this.cardSpacing),
-        Container(height: cardHeight, constraints: BoxConstraints(maxHeight: cardMaxHeight), child: _buildCardList(Data.lawInfoCardDataPortRow4)),
-        SizedBox(height: this.cardSpacing),
-        Container(height: cardHeight, constraints: BoxConstraints(maxHeight: cardMaxHeight), child: _buildCardList(Data.lawInfoCardDataPortRow5)),
-      ],
-    );
-  }
-
-  Widget _verticalAndLandWidgets() {
-    return Column(
-      children: <Widget>[
-        Container(height: cardHeight, constraints: BoxConstraints(maxHeight: cardMaxHeight), child: _buildCardList(Data.lawInfoCardDataLandRow1)),
-        SizedBox(height: this.cardSpacing),
-        Container(height: cardHeight, constraints: BoxConstraints(maxHeight: cardMaxHeight), child: _buildCardList(Data.lawInfoCardDataLandRow2)),
-        SizedBox(height: this.cardSpacing),
-        Container(height: cardHeight, constraints: BoxConstraints(maxHeight: cardMaxHeight), child: _buildCardList(Data.lawInfoCardDataLandRow3)),
-      ],
-    );
-  }
-
-  Widget _horizontalWidgets() {
-    return Container(
-        height: cardHeight,
-        constraints: BoxConstraints(maxHeight: cardMaxHeight),
-        child: Semantics(container: true, child: _buildCardList(Data.lawInfoCardData)));
+  Widget _buildCardsList(List<MenuCardData> cardsData) {
+    if (this.isVertical) {
+      return Padding(
+        padding: EdgeInsets.fromLTRB(Styles.spacing, 10, Styles.spacing, 0),
+        child: Wrap(
+          spacing: Styles.spacing,
+          runSpacing: Styles.spacing,
+          children: this._buildCards(cardsData, false),
+        ),
+      );
+    } else {
+      return Container(
+        height: this.cardHeight + Styles.spacing * 2.5,
+        child: ListView(
+          physics: BouncingScrollPhysics(),
+          padding: EdgeInsets.fromLTRB(Styles.spacing, Styles.spacing / 2, Styles.spacing, Styles.spacing * 2),
+          scrollDirection: Axis.horizontal,
+          children: this._buildCards(cardsData, true),
+        ),
+      );
+    }
   }
 }
