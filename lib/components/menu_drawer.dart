b1c12e17b4135a873df130f805610d65f5b1d510
Lee Chu Yem <leechuyem@gmail.com>
2019-09-10 18:50:23 +1000

    removed unused properties

--- lib/components/menu_drawer.dart
+++ lib/components/menu_drawer.dart
@@ -5,21 +5,21 @@ import 'package:dgha_brochure/misc/styles.dart';
 import 'package:flutter/material.dart';
 
 class MenuDrawer extends StatelessWidget {
-  final double borderRadius;
   final double width;
-  final double marginLeft;
-  final double iconSize;
 
-  MenuDrawer({this.width, this.borderRadius, this.marginLeft, this.iconSize});
+  MenuDrawer({this.width});
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: ClipRRect(
-        borderRadius: BorderRadius.only(topRight: Radius.circular(20), bottomRight: Radius.circular(20)),
+        borderRadius: BorderRadius.only(topRight: Radius.circular(Styles.normalRadius), bottomRight: Radius.circular(Styles.normalRadius)),
         child: Container(
           width: this.width,
-          constraints: BoxConstraints(maxWidth: 500),
+          constraints: BoxConstraints(
+            minWidth: 300,
+            maxWidth: 500,
+          ),
           child: Drawer(
             elevation: 20,
             semanticLabel: "Side bar menu",
@@ -27,7 +27,7 @@ class MenuDrawer extends StatelessWidget {
               padding: EdgeInsets.symmetric(horizontal: Styles.appBarHorizontalPadding),
               children: <Widget>[
                 SizedBox(
-                  height: this.iconSize / 3,
+                  height: Styles.iconSize / 3,
                 ),
                 MenuTile(tile: Data.guideDogInfoTileData),
                 MenuTile(tile: Data.guideDogAccessTileData),
