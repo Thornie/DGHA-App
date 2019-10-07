c83368bfe0b308de3c30c08db97077a33f858f9c
Lee Chu Yem <leechuyem@gmail.com>
2019-10-08 00:34:28 +1100

    add language detection
    - detect language using the popup menu selection

--- lib/misc/styles.dart
+++ lib/misc/styles.dart
@@ -9,23 +9,25 @@ class Styles {
 
   // ----------------- SIZES
   // ---------- APP
-  static final double appBarHorizontalPadding = 10; 
-  static final double normalRadius = 20;  
-  static final double imageMargin = 8; 
+  static final double appBarHorizontalPadding = 10;
+  static final double normalRadius = 20;
+  static final double imageMargin = 8;
   static final double spacing = 20;
   // ---------- TEXT
-  static final double textPadding = 10; 
+  static final double textPadding = 10;
   // ---------- ICON
-  static double iconSize = 24; 
-  static final double iconPadding = 10; 
-  static final double iconPaddingPadding = 10; 
-  
+  static double iconSize = 24;
+  static final double iconPadding = 10;
+  static final double iconPaddingPadding = 10;
+
   // ----------------- GENERAL TEXT
-  static TextStyle h1Style = TextStyle(fontFamily: "Poppins", fontWeight: FontWeight.w900, fontSize: 32, color: midnightBlue);       // DGHA
-  static TextStyle h2Style = TextStyle(fontFamily: "Poppins", fontWeight: FontWeight.w700, fontSize: 24, color: midnightBlue);       // Geneneral Info
-  static TextStyle h3Style = TextStyle(fontFamily: "Poppins", fontWeight: FontWeight.w600, fontSize: 20, color: midnightBlue);       // Cards Text
+  static TextStyle h1Style = TextStyle(fontFamily: "Poppins", fontWeight: FontWeight.w900, fontSize: 32, color: midnightBlue); // DGHA
+  static TextStyle h2Style = TextStyle(fontFamily: "Poppins", fontWeight: FontWeight.w700, fontSize: 24, color: midnightBlue); // Geneneral Info
+  static TextStyle h3Style = TextStyle(fontFamily: "Poppins", fontWeight: FontWeight.w600, fontSize: 20, color: midnightBlue); // Cards Text
   static TextStyle h4Style = TextStyle(fontFamily: "Manjari", fontWeight: FontWeight.w700, fontSize: 18, backgroundColor: yellow, color: midnightBlue);
-  static TextStyle h3LinkStyle = TextStyle(fontFamily: "Manjari", fontWeight: FontWeight.w700, fontSize: 20, color: midnightBlue);   // Drawer Tiles
-  static TextStyle pStyle = TextStyle(fontFamily: "Manjari", fontWeight: FontWeight.w700, fontSize: 18, color: midnightBlue);        // Normal Text
-  static TextStyle linkStyle = TextStyle(fontFamily: "Manjari", fontWeight: FontWeight.w700, fontSize: 18, backgroundColor: yellow, color: midnightBlue, decoration: TextDecoration.underline, decorationColor: midnightBlue);     // Links
+  static TextStyle h3LinkStyle = TextStyle(fontFamily: "Manjari", fontWeight: FontWeight.w700, fontSize: 20, color: midnightBlue); // Drawer Tiles
+  static TextStyle pStyle = TextStyle(fontFamily: "Manjari", fontWeight: FontWeight.w700, fontSize: 18, color: midnightBlue); // Normal Text
+  static TextStyle linkStyle =
+      TextStyle(fontFamily: "Manjari", fontWeight: FontWeight.w700, fontSize: 18, backgroundColor: yellow, color: midnightBlue, decoration: TextDecoration.underline, decorationColor: midnightBlue);
+  static TextStyle highlightText = TextStyle(fontFamily: "Manjari", fontWeight: FontWeight.w700, fontSize: 18, backgroundColor: yellow, color: midnightBlue);
 }
