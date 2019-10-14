dd56a50ec441ed3be1101f429c90d08dafcda1b8
Thornie <matthewthorne30@gmail.com>
2019-10-14 14:12:24 +1100

    added page

---
+++ lib/screens/rating_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RatingScreen extends StatefulWidget {
+  @override
+  _RatingScreenState createState() => _RatingScreenState();
+}
+
+class _RatingScreenState extends State<RatingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
