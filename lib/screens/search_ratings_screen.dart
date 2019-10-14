b3a1ee1dbd52cadc01956b5e139749d566022362
Thornie <matthewthorne30@gmail.com>
2019-10-14 14:55:58 +1100

    added pages

---
+++ lib/screens/search_ratings_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchRatingScreen extends StatefulWidget {
+  @override
+  _SearchRatingScreenState createState() => _SearchRatingScreenState();
+}
+
+class _SearchRatingScreenState extends State<SearchRatingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
