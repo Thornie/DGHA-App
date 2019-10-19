ea336e096395a94e6464318a59f0515ba66bdbaa
Thornie <39043379+Thornie@users.noreply.github.com>
2019-10-19 16:43:15 +1100

    added optional custom size and padding

--- lib/components/dgha_icon.dart
+++ lib/components/dgha_icon.dart
@@ -5,22 +5,35 @@ class DghaIcon extends StatelessWidget {
   final IconData icon;
   final Color iconColor;
   final Color backgroundColor;
+  final double size;
+  final double paddingPadding;
+  final double padding;
 
-  DghaIcon({this.icon, this.iconColor, this.backgroundColor});
+  DghaIcon({
+    this.icon,
+    this.iconColor,
+    this.backgroundColor,
+    this.size,
+    this.paddingPadding,
+    this.padding,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(Styles.iconPaddingPadding),
+      padding: EdgeInsets.all(paddingPadding != null
+          ? this.paddingPadding
+          : Styles.iconPaddingPadding),
       child: Container(
-        padding: EdgeInsets.all(Styles.iconPadding),
+        padding:
+            EdgeInsets.all(padding != null ? this.padding : Styles.iconPadding),
         decoration: BoxDecoration(
           color: this.backgroundColor,
           borderRadius: BorderRadius.all(Radius.circular(1000)),
         ),
         child: Icon(
           this.icon,
-          size: Styles.iconSize,
+          size: size != null ? size : Styles.iconSize,
           color: this.iconColor,
         ),
       ),
