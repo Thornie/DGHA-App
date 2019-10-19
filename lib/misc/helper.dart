f1c8464e11dbe1c66e8aab2f42977c04f4f12f14
Lee Chu Yem <leechuyem@gmail.com>
2019-10-20 03:29:09 +1100

    add helper string methods
    - format string for google places query
    - format string of the address

--- lib/misc/helper.dart
+++ lib/misc/helper.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class Helper {
   Future<String> loadTxtFiles(BuildContext _context, fileName) async {
     var string = await DefaultAssetBundle.of(_context).loadString(fileName);
-    return string; 
+    return string;
   }
 
   Future<List<String>> loadMd(BuildContext _context, fileName) async {
@@ -14,5 +14,22 @@ class Helper {
     List<String> lines = ls.convert(string);
 
     return lines;
+  }
+
+  String formatStringForQuery(String input) {
+    // remove white space
+    String formattedInput = input.trim();
+
+    // join with +
+    formattedInput = formattedInput.split(" ").join("+");
+
+    return formattedInput;
+  }
+
+  String formatAddress(String input) {
+    int indexOfState = input.indexOf("VIC");
+    String newInput = input.substring(0, indexOfState);
+
+    return newInput;
   }
 }
