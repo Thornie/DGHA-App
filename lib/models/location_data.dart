36d851d6cc516c9d911d1561e8013effadc28444
Thornie <39043379+Thornie@users.noreply.github.com>
2019-10-19 16:48:26 +1100

    added locationData object

---
+++ lib/models/location_data.dart
@@ -0,0 +1,16 @@
+class LocationData {
+  final String name;
+  final String address;
+  final double overallRating;
+  final double customerServiceRating;
+  final double amenitiesRating;
+  final double locationRating;
+
+  LocationData(
+      {this.name,
+      this.address,
+      this.overallRating,
+      this.customerServiceRating,
+      this.amenitiesRating,
+      this.locationRating});
+}
