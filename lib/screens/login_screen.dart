6e1d67f8981f874c653e301ddb6effa72ab6973f
Thornie <matthewthorne30@gmail.com>
2019-10-21 13:53:51 +1100

    restyled screens to match app theme

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,9 +1,11 @@
+import 'package:dgha_brochure/components/appbar.dart';
+import 'package:dgha_brochure/components/dgha_icon.dart';
+import 'package:dgha_brochure/misc/styles.dart';
 import 'package:dgha_brochure/screens/rating_menu_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
-import 'package:dgha_brochure/screens/register_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'login_screen';
@@ -24,175 +26,128 @@ class _LoginScreenState extends State<LoginScreen> {
       backgroundColor: Colors.white,
       body: ModalProgressHUD(
         inAsyncCall: showSpinner,
-        child: ListView(
-          children: <Widget>[
-            SizedBox(
-              height: 80,
-            ),
-            Text(
-              'Welcome to DGHA',
-              style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 32),
-              child: Material(
-                elevation: 2.0,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(30.0),
+        child: GestureDetector(
+          //Close the keyboard when user taps on screen
+          onTap: () {
+            FocusScope.of(context).requestFocus(new FocusNode());
+          },
+          child: ListView(
+            children: <Widget>[
+              DghaAppBar(
+                isMenu: true,
+                text: 'DGHA Login',
+                childOne: GestureDetector(
+                  child: DghaIcon(
+                    size: 35,
+                    padding: 4,
+                    backgroundColor: Styles.midnightBlue,
+                    iconColor: Styles.yellow,
+                    icon: Icons.close,
+                  ),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
                 ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32, vertical: 8.0),
                 child: TextField(
                   onChanged: (value) {
                     email = value;
                   },
-                  cursorColor: Colors.deepOrange,
                   decoration: InputDecoration(
-                      hintText: 'Type your Email',
-                      prefixIcon: Material(
-                        elevation: 0,
-                        borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                        child: Icon(
-                          Icons.email,
-                          color: Colors.blue,
-                        ),
-                      ),
-                      border: InputBorder.none,
-                      contentPadding:
-                          EdgeInsets.symmetric(horizontal: 25, vertical: 13)),
+                    hintText: 'Type your Email',
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Styles.midnightBlue,
+                    ),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 32),
-              child: Material(
-                elevation: 2.0,
-                borderRadius: BorderRadius.all(Radius.circular(30)),
+              SizedBox(
+                height: 15,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32, vertical: 8.0),
                 child: TextField(
                   onChanged: (value) {
                     password = value;
                   },
-                  cursorColor: Colors.deepOrange,
-                  obscureText: true,
                   decoration: InputDecoration(
-                      hintText: 'Type your password',
-                      prefixIcon: Material(
-                        elevation: 0,
-                        borderRadius: BorderRadius.all(Radius.circular(30)),
-                        child: Icon(
-                          Icons.lock,
-                          color: Colors.blue,
-                        ),
-                      ),
-                      border: InputBorder.none,
-                      contentPadding:
-                          EdgeInsets.symmetric(horizontal: 25, vertical: 13)),
+                    hintText: 'Type your Password',
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Styles.midnightBlue,
+                    ),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 32),
-              child: Container(
-                decoration: BoxDecoration(
+              SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32),
+                child: Container(
+                  decoration: BoxDecoration(
                     borderRadius: BorderRadius.all(Radius.circular(100)),
-                    color: Color(0xffff3a5a)),
-                child: FlatButton(
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w700,
-                        fontSize: 18),
+                    color: Styles.midnightBlue,
                   ),
-                  onPressed: () async {
-                    setState(() {
-                      showSpinner = true;
-                    });
-
-                    try {
-                      final user = await _auth.signInWithEmailAndPassword(
-                          email: email, password: password);
-                      if (user != null) {
-                        Navigator.pushNamed(context, RatingMenuScreen.id);
-                      }
+                  child: FlatButton(
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Styles.yellow,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 24,
+                      ),
+                    ),
+                    onPressed: () async {
                       setState(() {
-                        showSpinner = false;
+                        showSpinner = true;
                       });
-                    } catch (e) {
-                      print(e);
-                    }
-                  },
+
+                      try {
+                        final user = await _auth.signInWithEmailAndPassword(
+                            email: email, password: password);
+                        if (user != null) {
+                          Navigator.pushNamed(context, RatingMenuScreen.id);
+                        }
+                        setState(() {
+                          showSpinner = false;
+                        });
+                      } catch (e) {
+                        print(e);
+                      }
+                    },
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 40.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Don\'t have an Account ?',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 12,
-                      fontWeight: FontWeight.normal),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(context, RegisterScreen.id);
-                  },
-                  child: Text(
-                    'Sign Up ',
-                    style: TextStyle(
-                        color: Colors.red,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 15,
-                        decoration: TextDecoration.underline),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              'or',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 15.0, fontStyle: FontStyle.italic),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                FacebookSignInButton(
-                  onPressed: () {},
-                ),
-                SizedBox(
-                  height: 12.0,
-                ),
-                GoogleSignInButton(
-                  darkMode: true,
-                  onPressed: () {
-                    setState(() {});
-                  },
-                ),
-              ],
-            )
-          ],
+              //---------- Not using google or facebook to sign in yet
+              // SizedBox(
+              //   height: 20.0,
+              // ),
+              // Column(
+              //   crossAxisAlignment: CrossAxisAlignment.center,
+              //   children: <Widget>[
+              //     FacebookSignInButton(
+              //       onPressed: () {},
+              //     ),
+              //     SizedBox(
+              //       height: 12.0,
+              //     ),
+              //     GoogleSignInButton(
+              //       darkMode: true,
+              //       onPressed: () {
+              //         setState(() {});
+              //       },
+              //     ),
+              //   ],
+              // )
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,6 +1,10 @@
+import 'package:dgha_brochure/components/appbar.dart';
+import 'package:dgha_brochure/components/dgha_icon.dart';
+import 'package:dgha_brochure/misc/styles.dart';
 import 'package:dgha_brochure/screens/rating_menu_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:dgha_brochure/screens/login_screen.dart';
 
@@ -24,169 +28,144 @@ class _RegisterScreenState extends State<RegisterScreen> {
     return Scaffold(
       body: ModalProgressHUD(
         inAsyncCall: showSpinner,
-        child: ListView(
-          children: <Widget>[
-            SizedBox(
-              height: 30.0,
-            ),
-            CircleAvatar(
-              maxRadius: 50,
-              child: Icon(Icons.person_add),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 32, vertical: 8.0),
-              child: TextField(
-                onChanged: (value) {
-                  email = value;
-                },
-                cursorColor: Colors.deepOrange,
-                decoration: InputDecoration(
-                  hintText: 'Type your Email',
-                  prefixIcon: Material(
-                    elevation: 0,
-                    borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                    child: Icon(
-                      Icons.email,
-                      color: Colors.blue,
-                    ),
+        child: GestureDetector(
+          //Close the keyboard when user taps on screen
+          onTap: () {
+            FocusScope.of(context).requestFocus(new FocusNode());
+          },
+          child: ListView(
+            children: <Widget>[
+              DghaAppBar(
+                isMenu: true,
+                text: 'DGHA Register',
+                childOne: GestureDetector(
+                  child: DghaIcon(
+                    size: 35,
+                    padding: 4,
+                    backgroundColor: Styles.midnightBlue,
+                    iconColor: Styles.yellow,
+                    icon: Icons.close,
                   ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 15.0,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 32, vertical: 8.0),
-              child: TextField(
-                onChanged: (value) {
-                  password = value;
-                },
-                cursorColor: Colors.deepOrange,
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: 'Type your password',
-                  prefixIcon: Material(
-                    elevation: 0,
-                    borderRadius: BorderRadius.all(Radius.circular(30)),
-                    child: Icon(
-                      Icons.lock,
-                      color: Colors.blue,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 15.0,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 32, vertical: 8.0),
-              child: TextField(
-                onChanged: (value) {
-                  confirmpass = value;
-                },
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: 'Confirm password',
-                  prefixIcon: Material(
-                    elevation: 0,
-                    borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                    child: Icon(
-                      Icons.lock,
-                      color: Colors.blue,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Already have an Account ?',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 12,
-                      fontWeight: FontWeight.normal),
-                ),
-                GestureDetector(
                   onTap: () {
-                    Navigator.pushNamed(context, LoginScreen.id);
+                    Navigator.of(context).pop();
                   },
-                  child: Text(
-                    'LogIn ',
-                    style: TextStyle(
-                        color: Colors.red,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 15,
-                        decoration: TextDecoration.underline),
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-              child: Text.rich(TextSpan(children: [
-                TextSpan(
-                    text: 'By Clicking Sign Up you agree to the following '),
-                TextSpan(
-                    text: 'Terms and Conditions',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold, color: Colors.indigo)),
-              ])),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: RaisedButton(
-                padding: EdgeInsets.fromLTRB(40.0, 16.0, 30.0, 16.0),
-                color: Color(0xffff3a5a),
-                elevation: 0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30.0),
-                    bottomLeft: Radius.circular(30.0),
-                  ),
-                ),
-                onPressed: () async {
-                  setState(() {
-                    showSpinner = true;
-                  });
-                  try {
-                    final newUser = await _auth.createUserWithEmailAndPassword(
-                        email: email, password: password);
-                    if (newUser != null) {
-                      Navigator.pushNamed(context, RatingMenuScreen.id);
-                    }
-                    setState(() {
-                      showSpinner = false;
-                    });
-                  } catch (e) {
-                    print(e);
-                  }
-                },
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Text(
-                      'Sign Up'.toUpperCase(),
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 16.0),
-                    ),
-                  ],
                 ),
               ),
-            )
-          ],
+              SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32, vertical: 8.0),
+                child: TextField(
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Type your Email',
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Styles.midnightBlue,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32, vertical: 8.0),
+                child: TextField(
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Type your Password',
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Styles.midnightBlue,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32, vertical: 8.0),
+                child: TextField(
+                  onChanged: (value) {
+                    confirmpass = value;
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Confirm Password',
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Styles.midnightBlue,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                child: Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                          text:
+                              'By Clicking Sign Up you agree to the following '),
+                      TextSpan(
+                          text: 'Terms and Conditions',
+                          style: Styles.highlightText),
+                    ],
+                  ),
+                  style: Styles.pStyle,
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(100)),
+                    color: Styles.midnightBlue,
+                  ),
+                  child: FlatButton(
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        color: Styles.yellow,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 24,
+                      ),
+                    ),
+                    onPressed: () async {
+                      setState(() {
+                        showSpinner = true;
+                      });
+                      try {
+                        final newUser =
+                            await _auth.createUserWithEmailAndPassword(
+                                email: email, password: password);
+                        if (newUser != null) {
+                          Navigator.pushNamed(context, RatingMenuScreen.id);
+                        }
+                        setState(() {
+                          showSpinner = false;
+                        });
+                      } catch (e) {
+                        print(e);
+                      }
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
