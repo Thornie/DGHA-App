491551c2b8d701aa20f0f4cd88d8e060f545800b
Lee Chu Yem <leechuyem@gmail.com>
2019-10-22 00:30:00 +1100

    add review container

---
+++ lib/components/review_container.dart
@@ -0,0 +1,52 @@
+import 'package:dgha_brochure/misc/styles.dart';
+import 'package:dgha_brochure/screens/review_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:dgha_brochure/models/review.dart';
+
+class ReviewContainer extends StatelessWidget {
+  final Review review;
+
+  ReviewContainer({this.review});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            height: 2,
+            color: Styles.midnightBlue,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            child: Column(
+              children: <Widget>[
+                // ------ NOTE: Overall Review
+                LocationRatingBar(
+                  title: "Rated: ",
+                  rating: this.review.overallRating,
+                  isSmall: true,
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  // decoration: BoxDecoration(border: Border.all()),
+                  child: SelectableText(
+                    this.review.comment,
+                    style: Styles.pStyle,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: Styles.spacing,
+          ),
+        ],
+      ),
+    );
+  }
+}
