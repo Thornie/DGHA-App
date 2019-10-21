650faac6cd0d9d1a4ac2e8a9f127b982de709cf2
Lee Chu Yem <leechuyem@gmail.com>
2019-10-22 00:32:07 +1100

    add review class

---
+++ lib/models/review.dart
@@ -0,0 +1,21 @@
+class Review {
+  final String placeName;
+  final String placeId;
+  final String email;
+  final String comment;
+  final double overallRating;
+  final double locationRating;
+  final double custServRating;
+  final double amenitiesRating;
+
+  Review({
+    this.placeName,
+    this.placeId,
+    this.email,
+    this.comment,
+    this.overallRating,
+    this.locationRating,
+    this.custServRating,
+    this.amenitiesRating,
+  });
+}
