c8d5af96ff8b97b83669afddaada27c996cee412
Lee Chu Yem <leechuyem@gmail.com>
2019-10-22 00:31:18 +1100

    add route arguments for user rating screen

--- lib/misc/route_generator.dart
+++ lib/misc/route_generator.dart
@@ -1,3 +1,4 @@
+import 'package:dgha_brochure/models/review_scr_args.dart';
 import 'package:dgha_brochure/models/screen_args.dart';
 import 'package:dgha_brochure/screens/info_screens.dart';
 import 'package:dgha_brochure/screens/login_screen.dart';
@@ -9,19 +10,19 @@ import 'package:flutter/material.dart';
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
-    ScreenArguments screenArguments;
-    try {
-      screenArguments = settings.arguments;
-    } catch (e) {}
-
     switch (settings.name) {
       case LoginScreen.id:
         return MaterialPageRoute(builder: (_) => LoginScreen());
       case InfoScreen.id:
+        InfoScrArgs infoScrArgs;
+
+        try {
+          infoScrArgs = settings.arguments;
+        } catch (e) {}
         return MaterialPageRoute(
             builder: (_) => InfoScreen(
-                  appBarTitle: screenArguments.title,
-                  texts: screenArguments.texts,
+                  appBarTitle: infoScrArgs.title,
+                  texts: infoScrArgs.texts,
                 ));
       case MenuScreen.id:
         return MaterialPageRoute(builder: (_) => MenuScreen());
@@ -30,7 +31,16 @@ class RouteGenerator {
       case ReviewScreen.id:
         return MaterialPageRoute(builder: (_) => ReviewScreen(settings.arguments));
       case RatingScreen.id:
-        return MaterialPageRoute(builder: (_) => RatingScreen());
+        ReviewScrArgs reviewScrArgs;
+        try {
+          reviewScrArgs = settings.arguments;
+        } catch (e) {}
+
+        return MaterialPageRoute(
+            builder: (_) => RatingScreen(
+                  placeId: reviewScrArgs.placeId,
+                  placeName: reviewScrArgs.placeName,
+                ));
       default:
         return MaterialPageRoute(builder: (_) => MenuScreen());
     }
