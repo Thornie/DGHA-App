e62fcfef44638427ef2fa41b06607c99f7bea65e
Lee Chu Yem <leechuyem@gmail.com>
2019-10-26 09:56:53 +1100

    change UserInputTextField to stateful widget
    - to keep text when the textfield is unfocused

--- lib/components/input_textfield.dart
+++ lib/components/input_textfield.dart
@@ -2,32 +2,41 @@ import 'package:dgha_brochure/misc/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class UserInputTextField extends StatelessWidget {
+// The widget has to be stateful
+// So that the input in the textfield won't be cleared when it is unfocused
+
+class UserInputTextField extends StatefulWidget {
   final IconData prefixIcon;
   final String hintText;
   final bool obscureText;
   final Function(String) onChange;
-  final TextEditingController _txtController = new TextEditingController();
 
   UserInputTextField({this.prefixIcon, this.hintText, this.obscureText = false, this.onChange});
+
+  @override
+  _UserInputTextFieldState createState() => _UserInputTextFieldState();
+}
+
+class _UserInputTextFieldState extends State<UserInputTextField> {
+  final TextEditingController _txtController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: _txtController,
       onChanged: (value) {
-        onChange(value);
+        widget.onChange(value);
       },
       style: Styles.inputStyle,
       cursorColor: Styles.midnightBlue,
       cursorWidth: 5,
-      obscureText: obscureText,
+      obscureText: widget.obscureText,
       decoration: InputDecoration(
-          prefix: prefixIcon == null ? Padding(padding: EdgeInsets.only(left: 30)) : null,
-          prefixIcon: prefixIcon != null
+          prefix: widget.prefixIcon == null ? Padding(padding: EdgeInsets.only(left: 30)) : null,
+          prefixIcon: widget.prefixIcon != null
               ? Padding(
                   padding: EdgeInsets.only(left: 25, right: 20),
-                  child: Icon(prefixIcon),
+                  child: Icon(widget.prefixIcon),
                 )
               : null,
           suffix: GestureDetector(
@@ -53,7 +62,7 @@ class UserInputTextField extends StatelessWidget {
             borderRadius: BorderRadius.all(Radius.circular(50)),
             borderSide: BorderSide(color: Styles.midnightBlue, width: 3),
           ),
-          hintText: hintText,
+          hintText: widget.hintText,
           contentPadding: EdgeInsets.symmetric(vertical: 20)),
     );
   }
