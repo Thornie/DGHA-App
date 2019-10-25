fe25ea13c37d40d4c23460356744372c7131a0cc
Lee Chu Yem <leechuyem@gmail.com>
2019-10-26 05:40:05 +1100

    create input textfield

---
+++ lib/components/input_textfield.dart
@@ -0,0 +1,60 @@
+import 'package:dgha_brochure/misc/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class UserInputTextField extends StatelessWidget {
+  final IconData prefixIcon;
+  final String hintText;
+  final bool obscureText;
+  final Function(String) onChange;
+  final TextEditingController _txtController = new TextEditingController();
+
+  UserInputTextField({this.prefixIcon, this.hintText, this.obscureText = false, this.onChange});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: _txtController,
+      onChanged: (value) {
+        onChange(value);
+      },
+      style: Styles.inputStyle,
+      cursorColor: Styles.midnightBlue,
+      cursorWidth: 5,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+          prefix: prefixIcon == null ? Padding(padding: EdgeInsets.only(left: 30)) : null,
+          prefixIcon: prefixIcon != null
+              ? Padding(
+                  padding: EdgeInsets.only(left: 25, right: 20),
+                  child: Icon(prefixIcon),
+                )
+              : null,
+          suffix: GestureDetector(
+            onTap: () {
+              Future.delayed(Duration(milliseconds: 50)).then((_) {
+                _txtController.clear();
+              });
+            },
+            child: Padding(
+              padding: EdgeInsets.only(right: 25, left: 10),
+              child: Icon(
+                FontAwesomeIcons.times,
+                color: Styles.midnightBlue,
+                size: Styles.iconSize,
+              ),
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(50)),
+            borderSide: BorderSide(color: Styles.grey, width: 3),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(50)),
+            borderSide: BorderSide(color: Styles.midnightBlue, width: 3),
+          ),
+          hintText: hintText,
+          contentPadding: EdgeInsets.symmetric(vertical: 20)),
+    );
+  }
+}
