dc552d696d82a5d6edd969ac2bf5acf702279c16
Lee Chu Yem <leechuyem@gmail.com>
2019-10-26 20:52:42 +1100

    add service class for handling dynamic link

---
+++ lib/services/open_dynamic_link.dart
@@ -0,0 +1,26 @@
+import 'package:dgha_brochure/screens/register_screen.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+import 'package:flutter/material.dart';
+
+class OpenDynamicLink {
+  static void initDynamicLink(BuildContext context) async {
+    final PendingDynamicLinkData data = await FirebaseDynamicLinks.instance.getInitialLink();
+    final Uri deepLink = data?.link;
+
+    // When the app is not yet opened
+    if (deepLink != null) {
+      Navigator.of(context).pushNamed(RegisterScreen.id);
+    }
+
+    // When the app is opened
+    FirebaseDynamicLinks.instance.onLink(onSuccess: (PendingDynamicLinkData dynamicLink) async {
+      final Uri deepLink = dynamicLink?.link;
+
+      if (deepLink != null) {
+        Navigator.of(context).pushNamed(RegisterScreen.id);
+      }
+    }, onError: (OnLinkErrorException e) async {
+      print('onLinkError: ' + e.message);
+    });
+  }
+}
