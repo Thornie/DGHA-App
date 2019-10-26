997c958873c62aa9bf460216ea86ca47d12630ab
Lee Chu Yem <leechuyem@gmail.com>
2019-10-27 03:08:29 +1100

    implement shared preference
    - check for if the user uses the app for the first time

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:dgha_brochure/misc/route_generator.dart';
 import 'package:dgha_brochure/misc/styles.dart';
 import 'package:dgha_brochure/screens/explore_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:dgha_brochure/screens/login_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(DGHA());
 
@@ -10,8 +12,51 @@ class DGHA extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primaryColor: Styles.midnightBlue),
-      initialRoute: ExploreScreen.id,
       onGenerateRoute: RouteGenerator.generateRoute,
+      home: FirstScreen(),
     );
+  }
+}
+
+class FirstScreen extends StatefulWidget {
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  void initState() {
+    super.initState();
+    loadPage();
+  }
+
+  void loadPage() async {
+    var result = await loadLoginScr();
+    bool hasOpened = result[0] as bool;
+    SharedPreferences prefs = result[1] as SharedPreferences;
+
+    if (hasOpened) {
+      Future.delayed(Duration.zero, () {
+        Navigator.pushReplacementNamed(context, ExploreScreen.id);
+      });
+    } else {
+      await prefs.setBool('hasOpened', true);
+
+      Future.delayed(Duration.zero, () {
+        Navigator.pushReplacementNamed(context, LoginScreen.id);
+      });
+    }
+  }
+
+  Future<List<dynamic>> loadLoginScr() {
+    return SharedPreferences.getInstance().then((prefs) {
+      bool hasOpened = prefs.getBool('hasOpened') == null ? false : true;
+      return [hasOpened, prefs];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
