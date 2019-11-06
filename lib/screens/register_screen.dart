d10e1e0475f0a3a90e115c63dcd1c85e70389c41
Thornie <matthewthorne30@gmail.com>
2019-11-06 14:42:36 +1100

    added text field icons

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -5,6 +5,7 @@ import 'package:dgha_brochure/misc/dgha_api.dart';
 import 'package:dgha_brochure/misc/styles.dart';
 import 'package:dgha_brochure/screens/explore_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart';
 
 class RegisterScreen extends StatefulWidget {
@@ -92,6 +93,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                             child: Container(
                               child: UserInputTextField(
                                 keyboardType: TextInputType.emailAddress,
+                                prefixIcon: FontAwesomeIcons.solidEnvelope,
                                 hintText: "Email",
                                 onChange: (value) {
                                   this.email = value;
@@ -108,6 +110,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                             label: "Password text field",
                             child: Container(
                               child: UserInputTextField(
+                                prefixIcon: FontAwesomeIcons.lock,
                                 hintText: "Password",
                                 obscureText: true,
                                 onChange: (value) {
@@ -125,6 +128,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                             label: "Confirm password text field",
                             child: Container(
                               child: UserInputTextField(
+                                prefixIcon: FontAwesomeIcons.lock,
                                 hintText: "Confirm Password",
                                 obscureText: true,
                                 highlightRed: !passwordMatch,
