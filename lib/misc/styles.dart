64d358f14a9cb782049e2c8a2c17eb8c2668f032
Lee Chu Yem <leechuyem@gmail.com>
2019-11-12 23:09:00 +1100

    change variable to const and add a new blackTextStyle

--- lib/misc/styles.dart
+++ lib/misc/styles.dart
@@ -2,22 +2,22 @@ import 'package:flutter/material.dart';
 
 class Styles {
   // ----------------- COLOURS
-  static final Color yellow = Color(0xfffae100);
-  static final Color midnightBlue = Color(0xff00205b);
-  static final Color transMidnightBlue = Color(0x7000205b);
-  static final Color grey = Color(0xff576a80);
-  static final Color red = Color(0xffe05c41);
-  static final Color transparent = Color(0x00000000);
-  static final Color softGrey = Color(0xfff2f5f8);
-  static final Color mediumGrey = Color(0xff828282);
+  static const Color yellow = Color(0xfffae100);
+  static const Color midnightBlue = Color(0xff00205b);
+  static const Color transMidnightBlue = Color(0x7000205b);
+  static const Color grey = Color(0xff576a80);
+  static const Color red = Color(0xffe05c41);
+  static const Color transparent = Color(0x00000000);
+  static const Color softGrey = Color(0xfff2f5f8);
+  static const Color mediumGrey = Color(0xff828282);
 
   // ----------------- SIZES
   // ---------- APP
-  static final double appBarHorizontalPadding = 10;
-  static final double normalRadius = 20;
-  static final double imageMargin = 8;
-  static final double spacing = 20;
-  static final double heightFromAppBar = 80;
+  static const double appBarHorizontalPadding = 10;
+  static const double normalRadius = 20;
+  static const double imageMargin = 8;
+  static const double spacing = 20;
+  static const double heightFromAppBar = 80;
   // ---------- TEXT
   static final double textPadding = 10;
   // ---------- ICON
@@ -26,46 +26,49 @@ class Styles {
   static final double iconPaddingPadding = 10;
 
   // ----------------- NOTE: GENERAL TEXT
-  static TextStyle pStyle = TextStyle(
+  static const TextStyle pStyle = TextStyle(
     fontFamily: "Poppins",
     fontSize: 18,
     color: midnightBlue,
   );
 
   // ------ e.g. "DGHA"
-  static TextStyle h1Style = pStyle.copyWith(fontSize: 32, fontWeight: FontWeight.w900);
+  static final TextStyle h1Style = pStyle.copyWith(fontSize: 32, fontWeight: FontWeight.w900);
 
   // ------ e.g. "General Info"
-  static TextStyle h2Style = pStyle.copyWith(fontSize: 24, fontWeight: FontWeight.w700);
+  static final TextStyle h2Style = pStyle.copyWith(fontSize: 24, fontWeight: FontWeight.w700);
 
   // ------ e.g. "Federal"
-  static TextStyle h3Style = pStyle.copyWith(fontSize: 20, fontWeight: FontWeight.w600);
+  static final TextStyle h3Style = pStyle.copyWith(fontSize: 20, fontWeight: FontWeight.w600);
 
   // ------ e.g. "Customer Service"
-  static TextStyle h4Style = pStyle.copyWith(fontSize: 18, fontWeight: FontWeight.w600);
+  static final TextStyle h4Style = pStyle.copyWith(fontSize: 18, fontWeight: FontWeight.w600);
 
   // ------ Drawer Tiles Font
-  static TextStyle txtBtnStyle = h3Style.copyWith(fontWeight: FontWeight.w500);
+  static final TextStyle txtBtnStyle = h3Style.copyWith(fontWeight: FontWeight.w500);
 
   // ------ e.g. "Japanese"
-  static TextStyle highlightText = pStyle.copyWith(backgroundColor: yellow);
+  static final TextStyle highlightText = pStyle.copyWith(backgroundColor: yellow);
 
   // ------ e.g. "No Review"
-  static TextStyle boldPStyle = pStyle.copyWith(fontWeight: FontWeight.w600);
+  static final TextStyle boldPStyle = pStyle.copyWith(fontWeight: FontWeight.w600);
 
+  // ------
   // ------ link
-  static TextStyle linkStyle = highlightText.copyWith(
+  static final TextStyle linkStyle = highlightText.copyWith(
     decoration: TextDecoration.underline,
     decorationColor: midnightBlue,
     fontWeight: FontWeight.w500,
   );
 
   // ------ e.g. "Skip"
-  static TextStyle yellowTxtBtnStyle = pStyle.copyWith(
+  static final TextStyle yellowTxtBtnStyle = pStyle.copyWith(
     fontSize: 20,
     fontWeight: FontWeight.w700,
     color: yellow,
   );
+
+  static TextStyle blackTxtBtnStyle = yellowTxtBtnStyle.copyWith(color: Colors.black45);
 
   // ------ e.g. "Sign In"
   static TextStyle btnTextBlueUnderlineStyle = yellowTxtBtnStyle.copyWith(
