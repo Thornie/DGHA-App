23b4c2933c9286365a825e8dda9fa5f3278724a1
Lee Chu Yem <leechuyem@gmail.com>
2019-11-16 00:55:17 +1100

    Separate components from container to allow screen reader to read

--- lib/components/user_rating_container.dart
+++ lib/components/user_rating_container.dart
@@ -80,98 +80,103 @@ class _UserRatingContainerState extends State<UserRatingContainer> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                //----------Title
-                Text(
-                  widget.title,
-                  style: Styles.h2Style,
-                ),
-                SizedBox(
-                  width: 15,
-                ),
-                //----------More Info
-                Container(
-                  height: 50,
-                  child: Semantics(
-                    button: true,
-                    label: "Hint button",
-                    hint: "Double tap to open the hint pop up",
-                    excludeSemantics: true,
-                    child: GestureDetector(
-                      child: DghaIcon(
-                        icon: FontAwesomeIcons.question,
-                        iconColor: Styles.yellow,
-                        backgroundColor: Styles.midnightBlue,
-                        size: 30,
-                        padding: 6,
-                        paddingPadding: 4,
-                      ),
-                      onTap: () {
-                        createAlertDialog(context);
-                      },
+    return Semantics(
+      explicitChildNodes: true,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+
+          // ------------------------- NOTE: Title of Rating and Hint
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              //---------- NOTE: Title
+              Text(
+                widget.title,
+                style: Styles.h2Style,
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              //---------- NOTE: More Info
+              Container(
+                height: 50,
+                child: Semantics(
+                  button: true,
+                  label: "Hint button",
+                  hint: "Double tap to open the hint pop up",
+                  excludeSemantics: true,
+                  child: GestureDetector(
+                    child: DghaIcon(
+                      icon: FontAwesomeIcons.question,
+                      iconColor: Styles.yellow,
+                      backgroundColor: Styles.midnightBlue,
+                      size: 30,
+                      padding: 6,
+                      paddingPadding: 4,
                     ),
+                    onTap: () {
+                      createAlertDialog(context);
+                    },
                   ),
                 ),
-              ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            DghaStarRating(
-              height: MediaQuery.of(context).size.width / 7,
-              rating: widget.rating,
-              starCount: 5,
-              onTap: (rating) {
-                widget.rating = rating;
-              },
-            ),
-            SizedBox(
-              height: 35,
-            ),
-            //----------Next Button
-            GestureDetector(
-              onTap: widget.onPressed,
-              child: Semantics(
-                label: "Next",
-                hint: "Double tap to go to the next page",
-                excludeSemantics: true,
-                child: Container(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  padding: EdgeInsets.symmetric(vertical: 10),
-                  decoration: BoxDecoration(
-                    color: Styles.midnightBlue,
-                    borderRadius: BorderRadius.all(Radius.circular(50)),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Styles.grey,
-                        blurRadius: 3,
-                        offset: Offset(2, 3),
+              ),
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              SizedBox(
+                height: 20,
+              ),
+              DghaStarRating(
+                height: MediaQuery.of(context).size.width / 7,
+                rating: widget.rating,
+                starCount: 5,
+                onTap: (rating) {
+                  widget.rating = rating;
+                },
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              //----------Next Button
+              GestureDetector(
+                onTap: widget.onPressed,
+                child: Semantics(
+                  label: "Next",
+                  hint: "Double tap to go to the next page",
+                  excludeSemantics: true,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    decoration: BoxDecoration(
+                      color: Styles.midnightBlue,
+                      borderRadius: BorderRadius.all(Radius.circular(50)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Styles.grey,
+                          blurRadius: 3,
+                          offset: Offset(2, 3),
+                        ),
+                      ],
+                    ),
+                    child: Text(
+                      "Next",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Styles.yellow,
+                        fontFamily: "Poppins",
+                        fontWeight: FontWeight.w700,
+                        fontSize: 20,
                       ),
-                    ],
-                  ),
-                  child: Text(
-                    "Next",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Styles.yellow,
-                      fontFamily: "Poppins",
-                      fontWeight: FontWeight.w700,
-                      fontSize: 20,
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
-      ],
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
@@ -183,8 +188,7 @@ class CommentSection extends StatefulWidget {
   final String hintText;
   Color btnColor;
 
-  CommentSection(
-      {this.title, this.onPressed, this.controller, this.hintText = "", this.btnColor});
+  CommentSection({this.title, this.onPressed, this.controller, this.hintText = "", this.btnColor});
 
   @override
   _CommentSectionState createState() => _CommentSectionState();
@@ -296,8 +300,7 @@ class _CommentSectionState extends State<CommentSection> {
               //----------Comment Box
               Semantics(
                 label: "Comment",
-                hint:
-                    "Double tap to leave a comment on your experience at this location",
+                hint: "Double tap to leave a comment on your experience at this location",
                 excludeSemantics: true,
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 20),
