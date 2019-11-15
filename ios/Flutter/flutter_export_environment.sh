e398d0717692e0f2af3af46246d29460ea70def3
Lee Chu Yem <leechuyem@Lees-iMac.local>
2019-11-15 19:34:49 +1100

    idk

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,7 +2,7 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/leechuyem/Downloads/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/leechuyem/Projects/dgha"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/leechuyem/Projects/dgha/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/leechuyem/Downloads/flutter/bin/cache/artifacts/engine/ios"
