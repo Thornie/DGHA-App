42e6a4c4f741d4987c861955eb1aeab0e60e8947
Lee Chu Yem <leechuyem@Lees-iMac.local>
2019-11-15 19:31:22 +1100

    add the pop up request location

--- lib/services/place_service.dart
+++ lib/services/place_service.dart
@@ -4,30 +4,58 @@ import 'package:dgha/models/place.dart';
 import 'package:dgha/models/search_response.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
+import 'package:location_permissions/location_permissions.dart';
 
 // TODO: change the url to rootURL
 class PlaceService {
+
+  static Future<bool> getPermission() async {
+    // check if the user has already allowed for the app to use location
+    PermissionStatus permission = await LocationPermissions().checkPermissionStatus();
+    bool hasAsked = false;
+
+    // if the user has not yet allow the app to use location, the app will ask to request locaiton ONCE
+    while (permission != PermissionStatus.granted && hasAsked == false) {
+      permission = await LocationPermissions().requestPermissions();
+      hasAsked = true;
+    }
+
+    if (permission == PermissionStatus.granted) {
+      return true;
+    }
+
+    return false;
+  }
+
   static Future<String> getState() async {
-    Position pos = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.lowest);
-    List<Placemark> placemarks = await Geolocator().placemarkFromCoordinates(pos.latitude, pos.longitude);
-    String state = placemarks[0].administrativeArea;
-    return state;
+    bool isGranted = await getPermission();
+
+    if (isGranted) {
+      Position pos = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.lowest);
+      List<Placemark> placemarks = await Geolocator().placemarkFromCoordinates(pos.latitude, pos.longitude);
+
+      return placemarks[0].administrativeArea;
+    }
+    return null;
   }
 
   static Future<List<PlaceData>> getRecommendedPlaces() async {
-    // NOTE: this only works with a physical device
-    // final String state = await getState();
-    final String state = "Victoria";
+    // this only works with a physical device
+    final String state = await getState();
+    // final String state = "Victoria";
 
-    String url = '${Data.rootTestingUrl}/location/recommend?state=$state';
-    http.Response res = await http.get(url, headers: {"Accept": "application/json"});
+    if (state != null) {
+      String url = '${Data.rootTestingUrl}/location/recommend?state=$state';
+      http.Response res = await http.get(url, headers: {"Accept": "application/json"});
 
-    if(res.statusCode == 200) {
-      return PlaceData.decodePlaceDataList(res.body);
-    } else {
-      return List<PlaceData>(); 
+      if (res.statusCode == 200) {
+        print("something");
+        return PlaceData.decodePlaceDataList(res.body);
+      }
     }
-  } 
+    return List<PlaceData>();
+  }
+  
 
   static Future<SearchPlace> getSearchedPlaces(String query, String nextPageToken) async {
     String formattedQuery = Helper().formatStringForQuery(query);
