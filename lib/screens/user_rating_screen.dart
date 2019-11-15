e64cc90b09dbd7c82340c86dec9897d2f4343e6c
Lee Chu Yem <leechuyem@Lees-iMac.local>
2019-11-16 00:45:48 +1100

    remove gesturedetector that wraps the screen
    - this prevents the screen reader to read properly

--- lib/screens/user_rating_screen.dart
+++ lib/screens/user_rating_screen.dart
@@ -206,72 +206,66 @@ class _UserRatingScreenState extends State<UserRatingScreen> {
 
     return Scaffold(
       body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+        children: <Widget>[
+          //----------App Bar
+          Align(
+        alignment: Alignment.centerLeft,
         child: GestureDetector(
-          //Close the keyboard when uder taps on screen
+          child: Semantics(
+            button: true,
+            label: "Close",
+            hint: "Double tap to cancel your review",
+            excludeSemantics: true,
+            child: DghaIcon(
+              size: 35,
+              padding: 4,
+              backgroundColor: Styles.midnightBlue,
+              iconColor: Styles.yellow,
+              icon: Icons.close,
+            ),
+          ),
           onTap: () {
-            FocusScope.of(context).requestFocus(new FocusNode());
+            Navigator.pop(context, false);
           },
-          child: SingleChildScrollView(
-            child: Container(
-              height: MediaQuery.of(context).size.height,
-              child: Column(
-                children: <Widget>[
-                  //----------App Bar
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: GestureDetector(
-                      child: Semantics(
-                        button: true,
-                        label: "Close",
-                        hint: "Double tap to cancel your review",
-                        excludeSemantics: true,
-                        child: DghaIcon(
-                          size: 35,
-                          padding: 4,
-                          backgroundColor: Styles.midnightBlue,
-                          iconColor: Styles.yellow,
-                          icon: Icons.close,
-                        ),
-                      ),
-                      onTap: () {
-                        Navigator.pop(context, false);
-                      },
-                    ),
-                  ),
-                  //----------Loading Text
-                  buildLoadingWidget(),
-                  //----------Page Navigation
-                  Padding(
-                    padding: const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 20),
-                    child: RatingBreadcrumbs(
-                      maxNavPos: maxNavPos,
-                      currentNavPos: currentNavPos,
-                      iconColor: Colors.white,
-                      backgroundColor: Styles.grey,
-                      highlightedIconColor: Styles.yellow,
-                      highlightedBackgroundColor: Styles.midnightBlue,
-                      controller: pageController,
-                    ),
-                  ),
-                  //----------Rating Section
-                  Expanded(
-                    child: PageView(
-                      controller: pageController,
-                      physics: NeverScrollableScrollPhysics(),
-                      children: <Widget>[
-                        overallRatingScreen,
-                        customerServiceRatingScreen,
-                        amenitiesRatingScreen,
-                        locationRatingScreen,
-                        commentSectionScreen,
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: (MediaQuery.of(context).size.height - 230) / 2,
-                  ),
-                ],
-              ),
+        ),
+          ),
+          //----------Loading Text
+          buildLoadingWidget(),
+          //----------Page Navigation
+          Padding(
+        padding: const EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 20),
+        child: RatingBreadcrumbs(
+          maxNavPos: maxNavPos,
+          currentNavPos: currentNavPos,
+          iconColor: Colors.white,
+          backgroundColor: Styles.grey,
+          highlightedIconColor: Styles.yellow,
+          highlightedBackgroundColor: Styles.midnightBlue,
+          controller: pageController,
+        ),
+          ),
+          //----------Rating Section
+          Expanded(
+        child: PageView(
+          controller: pageController,
+          physics: NeverScrollableScrollPhysics(),
+          children: <Widget>[
+            overallRatingScreen,
+            customerServiceRatingScreen,
+            amenitiesRatingScreen,
+            locationRatingScreen,
+            commentSectionScreen,
+          ],
+        ),
+          ),
+          SizedBox(
+        height: (MediaQuery.of(context).size.height - 230) / 2,
+          ),
+        ],
             ),
           ),
         ),
